f7f4ae462ae804403bead6d3a7b3f97a4be95008
FlorianNAdam <64063379+FlorianNAdam@users.noreply.github.com>
2025-10-10 10:24:12 +0200

    add nix module

--- flake.nix
+++ flake.nix
@@ -18,7 +18,52 @@
       nixpkgs,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
+    {
+      nixosModules.persistent-evdev-rs =
+        {
+          config,
+          pkgs,
+          lib,
+          ...
+        }:
+        let
+          cfg = config.services.persistent-evdev-rs;
+          persistent-evdev-rs = self.packages.${pkgs.system}.persistent-evdev-rs;
+
+          settingsFormat = pkgs.formats.json { };
+
+          configFile = settingsFormat.generate "persistent-evdev-rs-config" {
+            cache = "/var/cache/persistent-evdev-rs";
+            devices = lib.mapAttrs (virt: phys: "/dev/input/by-id/${phys}") cfg.devices;
+          };
+        in
+        {
+          options.services.persistent-evdev-rs = {
+            enable = lib.mkEnableOption "virtual input devices that persist even if the backing device is hotplugged";
+
+            devices = lib.mkOption {
+              default = { };
+              type = with lib.types; attrsOf str;
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            systemd.services.persistent-evdev-rs = {
+              description = "Persistent evdev proxy";
+              wantedBy = [ "multi-user.target" ];
+
+              serviceConfig = {
+                Restart = "on-failure";
+                ExecStart = "${persistent-evdev-rs}/bin/persistent-evdev-rs ${configFile}";
+                CacheDirectory = "persistent-evdev-rs";
+              };
+            };
+
+            services.udev.packages = [ persistent-evdev-rs ];
+          };
+        };
+    }
+    // flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = (import nixpkgs) {
@@ -42,49 +87,6 @@
         };
       in
       {
-        nixosModules.persistent-evdev-rs =
-          {
-            config,
-            pkgs,
-            lib,
-            ...
-          }:
-          let
-            cfg = config.services.persistent-evdev-rs;
-
-            settingsFormat = pkgs.formats.json { };
-
-            configFile = settingsFormat.generate "persistent-evdev-rs-config" {
-              cache = "/var/cache/persistent-evdev-rs";
-              devices = lib.mapAttrs (virt: phys: "/dev/input/by-id/${phys}") cfg.devices;
-            };
-          in
-          {
-            options.services.persistent-evdev-rs = {
-              enable = lib.mkEnableOption "virtual input devices that persist even if the backing device is hotplugged";
-
-              devices = lib.mkOption {
-                default = { };
-                type = with lib.types; attrsOf str;
-              };
-            };
-
-            config = lib.mkIf cfg.enable {
-              systemd.services.persistent-evdev-rs = {
-                description = "Persistent evdev proxy";
-                wantedBy = [ "multi-user.target" ];
-
-                serviceConfig = {
-                  Restart = "on-failure";
-                  ExecStart = "${pkgs.persistent-evdev-rs}/bin/persistent-evdev-rs ${configFile}";
-                  CacheDirectory = "persistent-evdev-rs";
-                };
-              };
-
-              services.udev.packages = [ pkgs.persistent-evdev-rs ];
-            };
-          };
-
         packages = {
           inherit persistent-evdev-rs;
         };
